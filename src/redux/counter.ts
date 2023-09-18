fd7a2db4e26887138851cbf1105d1b0bf1d0c0d4
Vijay Singh <42995488+LeadVjSingh@users.noreply.github.com>
2023-09-18 20:17:53 +0530

    Update counter.ts

--- src/redux/counter.ts
+++ src/redux/counter.ts
@@ -4,6 +4,8 @@ const counterSlice = createSlice({
   name: 'update',
   initialState: 0,
   reducers: {
+    //  name of reducers 1. increament 2. decreament 3. setCustomValue. note: you can add more reducers .
+    // every reducer will get state, action , and in return it will return new state
     increment: (state, action: PayloadAction<number>) => state + action.payload,
     decrement: (state, action: PayloadAction<number>) => state - action.payload,
     setCustomValue: (state, action: PayloadAction<number>) => action.payload,
