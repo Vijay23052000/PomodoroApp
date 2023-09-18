0744fd583f49ec63b44b08717e0e85b9885ad471
Vijay Singh <42995488+LeadVjSingh@users.noreply.github.com>
2023-09-18 11:57:49 +0530

    Update README.md
    Update Counter.tsx
    
    Update App.tsx
    
    Update App.tsx
    
    Update App.tsx
    
    fix: updated with reduxtoolkit
    
    fix: added redux toolkit code

--- App.tsx
+++ App.tsx
@@ -1,18 +1,34 @@
-
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import DetailsScreen from './components/DetailsScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import DetailsScreen from './src/components/DetailsScreen';
+import {Provider} from 'react-redux';
+import store from './src/redux';
+import Counter from './src/components/demo/Counter';
+
 // import HomeScreen from './components/HomeScreen';
 const Stack = createNativeStackNavigator();
 
-function App(){
+function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Details">
-        {/* <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/> */}
-        <Stack.Screen name="Details" component={DetailsScreen} options={{ headerShown: false }}/>
-      </Stack.Navigator>
+      <Provider store={store}>
+        <Stack.Navigator initialRouteName="Counter">
+          {/* <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+           <Counter /> */}
+
+          <Stack.Screen
+            name="Counter"
+            component={Counter}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Details"
+            component={DetailsScreen}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 }
