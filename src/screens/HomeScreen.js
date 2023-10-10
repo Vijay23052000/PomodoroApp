768aeff3e5defe74f2c7327b7c83944c026011ac
Vijay Kumar <128240876+Vijay23052000@users.noreply.github.com>
2023-10-10 18:40:34 +0530

    update home screen code but facing little bit bug

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,6 +8,7 @@ import {
 import {useRoute} from '@react-navigation/native';
 import SplashScreen from 'react-native-splash-screen';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native'; // Import useFocu
 
 let initialPomodoro = 60;
 let initialShortBreak = 120;
@@ -18,34 +19,111 @@ const HomeScreen =  ({navigation}) => {
   const route = useRoute();
   const backgroundColor = route.params?.backgroundColor ?? '#0ca689';
 
-  initialPomodoro = route.params?.pomodoroTime ?? initialPomodoro;
-  initialShortBreak = route.params?.breakTime ?? initialShortBreak;
-  initialLongBreak = route.params?.longBreakTime ?? initialLongBreak;
-  initialCycle = route.params?.cycleCount ?? initialCycle;
+  // initialPomodoro = route.params?.pomodoroTime ?? initialPomodoro;
+  // initialShortBreak = route.params?.breakTime ?? initialShortBreak;
+  // initialLongBreak = route.params?.longBreakTime ?? initialLongBreak;
+  // initialCycle = route.params?.cycleCount ?? initialCycle;
 
 
 
 
   const [currentState, setCurrentState] = useState(1);
 
-  const [timer, setTimer] = useState(initialPomodoro); // 25 minutes in seconds
+  const [timer, setTimer] = useState(pomodoro); // 25 minutes in seconds
   const [timerType, setTimerType] = useState('POMODORO');
   const [cycleCount, setCycleCount] = useState(1);
   const [isRunning, setIsRunning] = useState(false);
 
+  const [pomodoro, setPomodoro] = useState(initialPomodoro);
+  const [BreakTime, setBreakTime] = useState(initialShortBreak);
+  const [longBreak, setLongBreak] = useState(initialLongBreak);
 
-  useEffect(() => {
-    AsyncStorage.getItem('pomodoroTime')
-      .then(value => {
-        if (value !== null) {
-          initialPomodoro = parseInt(value)
-          setTimer(parseInt(value))
-          console.log('pomodoroTime      ---1---check--tttt-initialPomodoro-', initialPomodoro);
-        }
-      })
-      .catch(error => {
-        console.error('Error pomodoroTime', error);
-      });
+  console.log("pomodoro---------",pomodoro)
+  console.log("BreakTime--------",BreakTime)
+  console.log("longBreak--------",longBreak)
+
+// const setTimerFuction = () => {
+//   setTimer(pomodoro)
+//   console.log("setTimerFuction",pomodoro)
+
+// }
+
+  const loadCount1 = async () => {
+    try {
+      const value1 = await AsyncStorage.getItem('pomodoroTime');
+      if (value1 !== null) {
+        setPomodoro(parseInt(value1));
+        initialPomodoro = parseInt(value1)
+        console.log("refresh",pomodoro/60)
+        
+          if(timerType ==='POMODORO'){
+            setTimer(pomodoro)
+        setTimerType('FOCUS TIME');
+
+          //   setTimer(parseInt(value1))
+          //   // setTimerFuction();
+          
+          }
+      }
+    } catch (error) {
+      console.error('Error loading count from AsyncStorage:', error);
+    }
+  };
+
+  const loadCount2 = async () => {
+    try {
+      const value2 = await AsyncStorage.getItem('shortBreakTime');
+      if (value2 !== null) {
+        setBreakTime(parseInt(value2));
+        initialShortBreak = parseInt(value2)
+        console.log("refresh2",BreakTime/60)
+          if(timerType==='SHORT BREAK'){
+            setTimer(parseInt(value2))
+            setTimerType('BREAK');
+          }
+      }
+    } catch (error) {
+      console.error('Error loading count from AsyncStorage:', error);
+    }
+  };
+
+  const loadCount3 = async () => {
+    try {
+      const value3 = await AsyncStorage.getItem('longBreakTime');
+      if (value3 !== null) {
+        setLongBreak(parseInt(value3));
+        initialLongBreak = parseInt(value3)
+        console.log("refresh3",longBreak/60)
+          if(timerType=='LONG BREAK'){
+            setTimer(parseInt(value3))
+            setTimerType('long  break');
+          }
+      }
+    } catch (error) {
+      console.error('Error loading count from AsyncStorage:', error);
+    }
+  };
+  // Refresh the count whenever the screen is focused
+  useFocusEffect(() => {
+    loadCount1();
+    loadCount2();
+    loadCount3();
+// toggleState();
+
+  });
+
+  // useEffect(() => {
+  //   AsyncStorage.getItem('pomodoroTime')
+  //     .then(value => {
+  //       if (value !== null) {
+  //         initialPomodoro = parseInt(value)
+  //         setTimer(parseInt(value))
+  //         console.log('pomodoroTime      ---vijay 1---check--', initialPomodoro);
+  //       }
+  //     })
+  //     .catch(error => {
+  //       console.error('Error pomodoroTime', error);
+  //     });
 
     // AsyncStorage.getItem('shortBreakTime')
     //   .then(value => {
@@ -67,37 +145,10 @@ const HomeScreen =  ({navigation}) => {
     //     console.error('Error longBreakTime: ', error);
     //   });
 
-    // AsyncStorage.getItem('backgroundColor')
-    //   .then(value => {
-    //     if (value !== null) {
-    //       setBgColor(value);
-    //     }
-    //   })
-    //   .catch(error => {
-    //     console.error('Error backgroundColor', error);
-    //   });
 
-    //   AsyncStorage.getItem('CycleCount')
-    //   .then(value => {
-    //     if (value !== null) {
-    //       setCycleCount(parseInt(value));
-    //     }
-    //   })
-    //   .catch(error => {
-    //     console.error('Error CycleCount', error);
-    //   });
-
-  }, []);
+  // }, []);
   useEffect(() => {
     SplashScreen.hide();
-    
-    // setPomodoro(parseInt(longBreakTime));
-    AsyncStorage.setItem('longBreakTime', "120").catch(error => {
-      console.error('Error pomodoroTime: ', error);
-    });
-        
-    console.log('pomodoroTime      ------check--tttt--', AsyncStorage.getItem('longBreakTime'));
-    
   }, []);
 
   useEffect(() => {
@@ -121,15 +172,15 @@ const HomeScreen =  ({navigation}) => {
 
         if (cycleCount === initialCycle) {
           setTimerType('LONG BREAK');
-          setTimer(initialLongBreak); // 30 minutes in seconds
+          setTimer(longBreak); // 30 minutes in seconds
           setCycleCount(1);
         } else {
           setTimerType('SHORT BREAK');
-          setTimer(initialShortBreak); // 5 minutes in seconds
+          setTimer(BreakTime); // 5 minutes in seconds
         }
       } else {
         setTimerType('POMODORO');
-        setTimer(initialPomodoro); // 25 minutes in seconds
+        setTimer(pomodoro); // 25 minutes in seconds
       }
     }
 
@@ -182,27 +233,28 @@ const HomeScreen =  ({navigation}) => {
     setIsRunning(false);
     {
       buttonText === 'POMODORO'
-        ? setTimer(initialPomodoro)
+        ? setTimer(pomodoro)
         : buttonText === 'SHORT BREAK'
-        ? setTimer(initialShortBreak)
-        : setTimer(initialLongBreak);
+        ? setTimer(BreakTime)
+        : setTimer(longBreak);
     }
 
     setCycleCount(1);
+    setTimerType('POMODORO')
   };
 
   const toggleState = () => {
     switch (currentState) {
       case 1:
-        setTimer(initialShortBreak);
+        setTimer(BreakTime);
         setTimerType('SHORT BREAK');
         break;
       case 2:
-        setTimer(initialLongBreak);
+        setTimer(longBreak);
         setTimerType('LONG BREAK');
         break;
       case 3:
-        setTimer(timer);
+        setTimer(pomodoro);
         setTimerType('POMODORO');
         break;
       default:
@@ -303,10 +355,10 @@ const HomeScreen =  ({navigation}) => {
                 <Text style={styles.buttonText}>
                   {buttonText}{' '}
                   {buttonText === 'POMODORO'
-                    ? initialPomodoro / 60
+                    ? pomodoro / 60
                     : buttonText === 'SHORT BREAK'
-                    ? initialShortBreak / 60
-                    : initialLongBreak / 60}{' '}
+                    ? BreakTime / 60
+                    : longBreak / 60}{' '}
                   MIN
                 </Text>
               </View>
